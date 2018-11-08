8e0dcd185039501d4feb2161be8471839f4f1378
raulserranodev <raulserranodev@gmail.com>
2018-11-08 13:53:47 +0100

    Las diapos hechas

--- Src/main.cpp
+++ Src/main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include "fmod.hpp" // para utilizar el wrapper C++
 #include "fmod_errors.h" // para manejo de errores
+#include <conio.h>
 
 
 /*
@@ -24,43 +25,114 @@ void ERRCHECK(FMOD_RESULT result) {
 	}
 }
 
+//Método para pausa o despausar el audio
+void TogglePaused(FMOD::Channel* channel) {
+	bool paused;
+	channel->getPaused(&paused);
+	channel->setPaused(!paused);
+}
+
 int main() {
+
 	//Inicialización básica
 	System *system;
-	FMOD_RESULT result;
-	result = System_Create(&system); // Creamos el objeto system
-	ERRCHECK(result);
+	FMOD_RESULT result = System_Create(&system); ERRCHECK(result);// Creamos el objeto system
 
 	// 128 canales (numero maximo que podremos utilizar simultaneamente)
-	result = system->init(128, FMOD_INIT_NORMAL, 0); // Inicializacion de FMOD
-	ERRCHECK(result);
+	result = system->init(128, FMOD_INIT_NORMAL, 0); ERRCHECK(result);// Inicializacion de FMOD
+
 
 	//Carga de un sonido
 	Sound *sound1 = nullptr;
+
 	result = system->createSound(
-		"../../Media/muestras/piano.ogg", // path al archivo de sonido
-		FMOD_DEFAULT, // valores (por defecto en este caso: sin loop, 2D)
+		"../../Media/muestras/piano.ogg",
+		FMOD_DEFAULT | FMOD_LOOP_NORMAL, // valores (por defecto: sin loop, 2D) Existen más alternativas
 		0, // informacion adicional (nada en este caso)
-		&sound1); // handle al buffer de sonido
+		&sound1);
+
 
 	//Asignación de sonido a un canal
-	Channel *channel = nullptr;
-	result = system->playSound(
-		sound1, // buffer que se "engancha" a ese canal
-		0, // grupo de canales, 0 sin agrupar (agrupado en el master)
-		false, // arranca sin "pause" (se reproduce directamente)
-		&channel); // devuelve el canal que asigna
-		// el sonido ya se esta reproduciendo
+	Channel *channel = nullptr; //Hereda de ChannelControl(param de los canales)
 
+	//Clases adicionales
+	//Posibles clases adicionales
+	//ChannelGroup --> canal virtual para agrupar varios canales
+	//DSP--> filtros, mezclas, reber
+	//Geometry --> simulación de recintos acústicos
+	//Reber3D //Clases adicionales
+
+	//Reproducción del sonido
+	result = system->playSound(
+		sound1,
+		0, // grupo de canales, 0 sin agrupar (agrupado en el master)
+		true, // arranca con "pause" 
+		&channel);
+
+
+	//Modificación volumen
 	result = channel->setVolume(0.7f);
 
-	while (true)
-		result = system->update();
+	//Posiciona en el tiempo 
+	channel->setPosition(0, FMOD_TIMEUNIT_MS); //Medio segundo después
 
+	// se repite indefinidamente
+	channel->setLoopCount(-1);
+	// Se repite una sola vez
+	//channel->setLoopCount(0);
+	// Tres veces
+	//channel->setLoopCount(2);
+
+	//channel->setMute(true); Silencia/activa el canal.
+
+	//Modifica el volumen de sonidos multicanal. Por ejemplo en un sonido estéreo
+	//channel->setMixLevelsOutput(frontleft, frontright, center, ..., backleft, backright);
+	//channel->setPicth(2.0f);
+	//channel->setFrequency(rateHz);
+
+	TogglePaused(channel);
+
+	//Bucle principal
+	printf("[P] Pausar/Despausar\n[V/v] Subir/bajar volumen\n[Q] Salir\n");
+
+	bool paused; float volume = 1.0;
+	while (true) {
+		if (_kbhit())
+		{
+			int key = _getche();
+			if ((key == 'P') || (key == 'p'))
+			{
+				result = channel->getPaused(&paused); ERRCHECK(result);
+				result = channel->setPaused(!paused); ERRCHECK(result);
+			}
+			else if (key == 'V')
+			{
+				if (volume < 1.0)
+				{
+					volume = volume + 0.1;
+					result = channel->setVolume(volume); ERRCHECK(result);
+					printf("Volume: %f\n", volume);
+				}
+			}
+			else if (key == 'v')
+			{
+				if (volume > 0) {
+					volume = volume - 0.1;
+					result = channel->setVolume(volume); ERRCHECK(result);
+					printf("Volume: %f\n", volume);
+				}
+			}
+			else if ((key == 'Q') || (key == 'q')) break;
+
+		}		result = system->update();
+	}
+
+	//Libera el buffer de sonido
+	result = sound1->release(); ERRCHECK(result);
 
 	//Cierra el sistema
-	result = system->release();
-	ERRCHECK(result);
+	result = system->release(); ERRCHECK(result); //Libera el sonido
+
 
 	return 0;
 }
